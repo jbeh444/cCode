c346f40fa2b60b6e0096454f0f91f539da7f3bd0
jbeh444 <112517727+jbeh444@users.noreply.github.com>
2024-02-28 16:49:58 -0700

    Fun with c code

--- Operators.c
+++ Operators.c
@@ -1,5 +1,4 @@
-  
-// C program to illustrate the bitwise operators
+// C program to illustrate the arithmatic operators
 #include <stdio.h>
  
 int main()
@@ -8,12 +7,15 @@ int main()
     int a = 25, b = 5;
  
     // using operators and printing results
-    printf("a & b: %d\n", a & b);
-    printf("a | b: %d\n", a | b);
-    printf("a ^ b: %d\n", a ^ b);
-    printf("~a: %d\n", ~a);
-    printf("a >> b: %d\n", a >> b);
-    printf("a << b: %d\n", a << b);
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a % b = %d\n", a % b);
+    printf("+a = %d\n", +a);
+    printf("-a = %d\n", -a);
+    printf("a++ = %d\n", a++);
+    printf("a-- = %d\n", a--);
  
     return 0;
 }
